d7126c7d6732377158811045d2b1f816dc564aed
Rosa Linda <rossalindaperez16@gmail.com>
2025-01-23 00:02:41 -0300

    Atualizando o curso, utilização do for

--- src/estdcontrole/instfor.go
+++ src/estdcontrole/instfor.go
@@ -3,15 +3,30 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("1")
-	fmt.Println("2")
-	fmt.Println("3")
-	fmt.Println("4")
-	fmt.Println("5")
-	fmt.Println("6")
-	fmt.Println("7")
-	fmt.Println("8")
-	fmt.Println("9")
-	fmt.Println("10")
+	numero := 5
+
+	for i := 1; i <= 10; i++ {
+		resultado := numero * i
+		fmt.Printf("%d x %d = %d\n", numero, i, resultado)
+
+	}
+}
+
+/*  outra forma de usar o for:
+
+func main() {
+	for i:= 1; i <=10; i++{
+		fmt.Println(i)
+	}
+
 
 }
+
+
+
+i := 1
+	for i <= 10 {
+		fmt.Println(i)
+		i = i + 1
+	}
+*/
