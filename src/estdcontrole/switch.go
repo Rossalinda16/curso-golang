52df88fd9cefaebe65c2b2e46f2246e4005e1929
Rosa Linda <rossalindaperez16@gmail.com>
2025-02-18 23:25:21 -0300

    Adicionando arquivos ao curso Go da DIO

---
+++ src/estdcontrole/switch.go
@@ -0,0 +1,20 @@
+package main 
+
+import "fmt"
+
+func main() {
+
+	for i := 0; i <=10; i++ {
+
+		switch i{
+		case  0: fmt.Println("Cero")
+		case  1: fmt.Println("Un")
+		case  2: fmt.Println("Dos")
+		case  3: fmt.Println("Tres")
+		case  4: fmt.Println("Cuatro")
+		case  5: fmt.Println("Cinco")
+
+		}
+		
+	}
+}
